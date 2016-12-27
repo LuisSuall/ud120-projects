eff5cb684e16d75367c3fee6dcbff3e516378380
Luis Suárez <luissuall@gmail.com>
2016-12-27 14:18:17 +0100

    Complete DT Mini-Project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,8 +24,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+clf = DecisionTreeClassifier(min_samples_split=40)
+
+print "no of features:", len(features_train[0])
+
+print "Training model"
+time1 = time()
+clf.fit(features_train, labels_train)
+time2 = time()
+print "Training time:",time2-time1,"s"
+
+print "Predicting test data"
+time1 = time()
+pred = clf.predict(features_test)
+time2 = time()
+print "Predicting time:",time2-time1,"s"
+
+print "Computing accuracy"
+time1 = time()
+accuracy = accuracy_score(labels_test, pred)
+time2 = time()
+print "Accuracy time:",time2-time1,"s"
+
+print "Accuracy: "
+print accuracy
 
 
 #########################################################
-
-
