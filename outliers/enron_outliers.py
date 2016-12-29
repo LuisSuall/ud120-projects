f853e8431c0dfae911b0d4aa437021f0a6a85160
Luis Suárez <luissuall@gmail.com>
2016-12-29 17:27:07 +0100

    Complete outliers Mini-Project

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -2,6 +2,7 @@
 
 import pickle
 import sys
+import math
 import matplotlib.pyplot
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
@@ -9,11 +10,20 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop("TOTAL")
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
+for key, value in data_dict.iteritems():
+    if value["bonus"]>5000000 and not math.isnan(float(value["bonus"])):
+        print key, value["bonus"]
 ### your code below
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -7,14 +8,23 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    errors = np.power(predictions - net_worths,2)
 
-    
+    order = np.argsort(errors, axis = 0)
+
+    # Keep the 90% best fitted results
+    order = order[:int(len(order)*0.9)]
+
+    for idx in order:
+        cleaned_data.append([ages[idx],
+                             net_worths[idx],
+                             errors[idx]])
+
     return cleaned_data
-
