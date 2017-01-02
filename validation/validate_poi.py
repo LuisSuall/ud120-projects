9b93dca9ab2996f84586978ffa1bfc7dcde6b472
Luis Suárez <luissuall@gmail.com>
2017-01-02 18:15:10 +0100

    Complete Validation Mini-Project

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,6 +12,9 @@
 
 import pickle
 import sys
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -24,9 +27,14 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels,test_size=0.3, random_state = 42)
+### it's all yours from here forward!
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
-### it's all yours from here forward!  
+score = accuracy_score(labels_test, pred)
 
-
+print "Score:", score
